f8c6a73adf94b249ee6ec8a3557be548c58b9e89
Oroz <orozmamat.arzymamatov@gmail.com>
2026-01-09 11:37:18 +0600

    мен поект ÐÑмен проект коштум

--- bot.py
+++ bot.py
@@ -1,5 +1,23 @@
 import telebot
 
-token = ""
+token = "8366099038:AAHmc9YxHLtfhiXaEGkUmpphTd65lYCQvIk"
 
 bot = telebot.TeleBot(token)
+
+@bot.message_handler(func=lambda message: True)
+def say_hello(message):
+    text = message.text.lower()
+    if text == "Салам":
+      bot.send_message(message.chat.id, "Салам кандайсын")
+    elif text == "жакшы озун":
+      bot.send_message(message.chat.id,"менда жакшы")
+    elif text == "менде мындай суроо бар":
+        bot.send_message(message.chat.id,"суроонузду бериниз")
+    elif text == "дуйнодогу эн чон дарыя ":
+        bot.send_message(message.chat.id,"дуйнодогу эн чон дарыя нил дарыясы")
+    elif text == "id":
+        bot.message_handler(message.chat.id, message.chat.id)
+    else:
+        bot.send_message(message.chat.id,f"{message.text}")
+
+bot.polling(none_stop=True)
