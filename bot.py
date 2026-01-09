b78d3c00d3626cb52b82e7b69c15cb8d096a04fd
eliza <bolotbekkyzyeliza0@gmail.com>
2026-01-09 11:48:57 +0600

    men koshuldum

--- bot.py
+++ bot.py
@@ -1,5 +1,20 @@
 import telebot
 
-token = ""
+token = "8152166324:AAEFFn2vEqiDIyHlSEHVPbvlR5yLX-BcuBk"
 
 bot = telebot.TeleBot(token)
+import random
+
+jokes = [
+    "Программисттер кофе ичпесе, алар да error чыгарат 😄",
+    "Почему программисты путают Хэллоуин и Рождество? Потому что Oct 31 = Dec 25 🎃🎄",
+    "Debugging — ошол убакыт, качан код иштейт, бирок сен эмне кылганыңды түшүнбөйсүң 🤯",
+    "Почему компьютер холодный? Потому что он работает в Windows 🪟💻",
+    "Stack Overflow'у колдобосоң — Python'до жоголосуң 🐍"
+]
+@bot.message_handler(commands=['joke'])
+def send_joke(message):
+    random_joke = random.choice(jokes)
+    bot.send_message(message.chat.id, random_joke)
+
+bot.polling(none_stop=True)
